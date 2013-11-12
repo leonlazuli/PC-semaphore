bafe3a64eafcc36979f3866adf7d43b737922b01
leon-lazuli <leonlazuli@LiuLeontekiMacBook-Air.local>
2013-11-11 21:53:44 -0800

    after using c++
    af

--- 301241917.cpp
+++ 301241917.cpp
@@ -14,6 +14,42 @@ typedef int buffer_item;
 #define RAND_DIVISOR 100000000
 #define TRUE 1
 
+class StackBuffer  //a stack buffer to store the material
+{
+private:
+	int size;
+	int current;
+	int * ary;
+public:
+	Stack(int s):size(s),current(0){ary = new int[size];}
+	~Stack(){delete ary;}
+	void push(int n)
+	{
+		if(current == size)
+		{
+			cout << "********** error: try to push in to the full buffer ************";
+			exit(1);
+		}
+		ary[current++] = n;
+	}	
+	int pop()
+	{
+		if(current == 0)
+		{
+			cout << "*************error: try to pop from a empty buffer *****************";
+			exit(1);
+		}
+		return ary[--current]; 
+	}	
+	void showStack()
+	{
+		cout << "stack: ";
+		for(int i = 0; i != current; i++)
+			cout << ary[i] << ' ';
+		cout << endl;
+	}	
+};
+
 /* The mutex lock */
 pthread_mutex_t mutex;
 
@@ -60,34 +96,35 @@ void *producer(void *param)
 	int producerID = *(int*)param;
 	while(TRUE) {
 		/* sleep for a random period of time */
-		printf("Now in producer:%d \n",producerID);
-		int rNum = rand() / RAND_DIVISOR;
+		printf("Now in producer%d, sleep for a while\n",producerID);
+		int rNum = (rand() / RAND_DIVISOR)%100;
 		sleep(rNum);
-		printf("produer %d finish sleep.\n",producerID);
+		printf("produer%d finish sleep.\n",producerID);
 
 		/* generate a random number */
 		item++;
 
 		/* acquire the empty lock */
 		sem_wait(&empty);
-		printf("produer %d finsh to down the semapare.\n", producerID);
+		printf("produer%d finsh to down the semapare.\n", producerID);
 		/* acquire the mutex lock */
 		pthread_mutex_lock(&mutex);
-		printf("producer %d enter the critical section.\n", producerID);
+		printf("producer%d enter the critical section.\n", producerID);
 
 		if(insert_item(item)) {
-			fprintf(stderr, " Producer %d report error condition\n", producerID);
+			fprintf(stderr, " Producer%d report error condition\n", producerID);
 		}
 		else {
-			printf("producer %d produced %d\n", producerID, item);
+			printf("producer%d produced item%d\n", producerID, item);
 		}
 		/* release the mutex lock */
 		pthread_mutex_unlock(&mutex);
-		printf("produer %d exits the critical section, and try to up the semephore\n",producerID);
+		printf("produer%d exits the critical section, and try to up the semephore\n",producerID);
 		/* signal full */
 		sem_post(&full);
-		printf("producer %d finish up the semephore",producerID);
+		printf("producer%d finish up the semephore",producerID);
 	}
+	return NULL;
 }
 
 /* Consumer Thread */
@@ -96,33 +133,34 @@ void *consumer(void *param) {
 	int consumerID = *(int*)param;
 	while(TRUE) {
 		/* sleep for a random period of time */
-		printf("now in consumer:%d/n",consumerID);
-		int rNum = rand() / RAND_DIVISOR;
+		printf("now in consumer%d\n, sleep for a while",consumerID);
+		int rNum = (rand() / RAND_DIVISOR)%100;
 		sleep(rNum);
-		printf("comsumer %d finish sleep\n",consumerID);
+		printf("comsumer%d finish sleep\n",consumerID);
 		
 
 		/* aquire the full lock */
-		printf("consumer %d try to down the semaphore\n", consumerID);
+		printf("consumer%d try to down the semaphore\n", consumerID);
 		sem_wait(&full);
-		printf("consumer %d finish downing the semaphore\n",consumerID);
+		printf("consumer%d finish downing the semaphore\n",consumerID);
 		/* aquire the mutex lock */
 		pthread_mutex_lock(&mutex);
-		printf("consumer %d enter the critial section\n",consumerID);
+		printf("consumer%d enter the critial section\n",consumerID);
 		if(remove_item(&item)) {
-			fprintf(stderr, "Consumer %d report error condition\n", consumerID);
+			fprintf(stderr, "Consumer%d report error condition\n", consumerID);
 		}
 		else {
-			printf("consumer %d consumed %d\n", consumerID,item);
+			printf("consumer%d consumed %d\n", consumerID,item);
 		}
 		/* release the mutex lock */
 		pthread_mutex_unlock(&mutex);
-		printf("consumer %d exit the critical section\n",consumerID);
+		printf("consumer%d exit the critical section\n",consumerID);
 		/* signal empty */
-		printf("consumer %d try to up the semaphore\n",consumerID);
+		printf("consumer%d try to up the semaphore\n",consumerID);
 		sem_post(&empty);
-		printf("consumer %d finish uping the semaphore\n",consumerID);
+		printf("consumer%d finish uping the semaphore\n",consumerID);
 	}
+	return NULL;
 }
 
 /* Add an item to the buffer */
@@ -169,6 +207,7 @@ int main(int argc, char *argv[]) {
 	/* Initialize the app */
 	initializeData();
 
+	printf("******* now in main, before create the producer\n");
 	/* Create the producer threads */
 	static int proCounter = 0;
 	for(i = 0; i < numProd; i++) {
@@ -176,7 +215,7 @@ int main(int argc, char *argv[]) {
 		pthread_create(&tid,&attr,producer,&proCounter);
 		proCounter++;
 	}
-
+	printf("******** now in main, finish create the producer, before create the consumer\n");
 	/* Create the consumer threads */
 	static int conCounter = 0;
 	for(i = 0; i < numCons; i++) {
@@ -186,6 +225,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Sleep for the specified amount of time in milliseconds */
+	printf("********* now in main , finish create consumer and before main sleep\n");
 	sleep(mainSleepTime);
 
 	/* Exit the program */
