185b9e5d6b15fa0d2ae2b3fdd1e41c3bd60f6912
leon-lazuli <leonlazuli@d207-023-184-031.wireless.sfu.ca>
2013-11-18 15:06:12 -0800

    test the errors

--- 301241917.cpp
+++ 301241917.cpp
@@ -64,9 +64,15 @@ pthread_attr_t attr; //Set of thread attributes
 
 void show_material_total()
 {
-	printf("There are %d material_1 have been generated",g_material_1_count);
-	printf("There are %d material_2 have been generated",g_material_2_count);
-	printf("There are %d material_3 have been generated",g_material_3_count);
+	printf("There are %d material_1 have been generated\n",g_material_1_count);
+	printf("There are %d material_2 have been generated\n",g_material_2_count);
+	printf("There are %d material_3 have been generated\n",g_material_3_count);
+}
+
+void show_deadlock()
+{
+	printf("There occurs %d times deadlock because of inputBuffer\n",g_inputBufferDeadlockCounter);
+	printf("There occurs %d times deadlock because of outputQueue\n",g_outputQueueDeadlockCounter);
 }
 
 class InputBuffer  //a stack buffer to store the Material
@@ -96,11 +102,20 @@ public:
 		if(current < size)
 		{
 			if(item == MATERIAL_1)
+			{
 				m1_counter++;
+				++g_material_1_count;
+			}
 			else if(item == MATERIAL_2)
+			{	
 				m2_counter++;
+				++g_material_2_count;
+			}
 			else if(item == MATERIAL_3)
+			{
 				m3_counter++;
+				++g_material_3_count;
+			}
 			else
 			{
 				printf("********* error: trying to put undefined material into InputBuffer\n \n"); // this line is not necessary can be removed in the future
@@ -121,77 +136,78 @@ public:
 		}
 		else 
 		{ /* Error the buffer is full */
+			printf("*********** error: try to push into a full ary\n\n\n");
 			return -1;
 		}
 		
 		
 	}	
 	
-	Material try_pop_m1()
-	{
-		if(current > 0)
-		{
-			for(int i = 0; i != size; i++)
-			{
-				if(ary[i] == MATERIAL_1)
-				{
-					ary[i] = -1;
-					current--;
-					return MATERIAL_1;
-				}
-			}
-			return -1; // means no MATERIAL_1 is found
-		}
-		else
-		{
-			printf("*********** error: try to pop from a empty ary\n \n");
-			return -1; // error occurs 
-		}
-	}
-	
-	Material try_pop_m3()
-	{
-		if(current > 0)
-		{
-			for(int i = 0; i != size; i++)
-			{
-				if(ary[i] == MATERIAL_3)
-				{
-					ary[i] = -1;
-					current--;
-					return MATERIAL_3;
-				}
-			}
-			return -1; // means no MATERIAL_1 is found
-		}
-		else
-		{
-			printf("*********** error: try to pop from a empty ary\n \n");
-			return -1; // error occurs 
-		}
-	}
-	
-	Material try_pop_m2()
-	{
-		if(current > 0)
-		{
-			for(int i = 0; i != size; i++)
-			{
-				if(ary[i] == MATERIAL_2)
-				{
-					ary[i] = -1;
-					current--;
-					return MATERIAL_2;
-				}
-			}
-			return -1; // means no MATERIAL_1 is found
-		}
-		else
-		{
-			printf("*********** error: try to pop from a empty ary\n \n");
-			return -1; // error occurs 
-		}
-	}
+	// Material try_pop_m1()
+	// {
+	// 	if(current > 0)
+	// 	{
+	// 		for(int i = 0; i != size; i++)
+	// 		{
+	// 			if(ary[i] == MATERIAL_1)
+	// 			{
+	// 				ary[i] = -1;
+	// 				current--;
+	// 				return MATERIAL_1;
+	// 			}
+	// 		}
+	// 		return -1; // means no MATERIAL_1 is found
+	// 	}
+	// 	else
+	// 	{
+	// 		printf("*********** error: try to pop from a empty ary\n \n");
+	// 		return -1; // error occurs 
+	// 	}
+	// }
+	// 
+	// Material try_pop_m3()
+	// {
+	// 	if(current > 0)
+	// 	{
+	// 		for(int i = 0; i != size; i++)
+	// 		{
+	// 			if(ary[i] == MATERIAL_3)
+	// 			{
+	// 				ary[i] = -1;
+	// 				current--;
+	// 				return MATERIAL_3;
+	// 			}
+	// 		}
+	// 		return -1; // means no MATERIAL_1 is found
+	// 	}
+	// 	else
+	// 	{
+	// 		printf("*********** error: try to pop from a empty ary\n \n");
+	// 		return -1; // error occurs 
+	// 	}
+	// }
+	// 
+	// Material try_pop_m2()
+	// {
+	// 	if(current > 0)
+	// 	{
+	// 		for(int i = 0; i != size; i++)
+	// 		{
+	// 			if(ary[i] == MATERIAL_2)
+	// 			{
+	// 				ary[i] = -1;
+	// 				current--;
+	// 				return MATERIAL_2;
+	// 			}
+	// 		}
+	// 		return -1; // means no MATERIAL_1 is found
+	// 	}
+	// 	else
+	// 	{
+	// 		printf("*********** error: try to pop from a empty ary\n \n");
+	// 		return -1; // error occurs 
+	// 	}
+	// }
 	
 	bool try_get_materials(Product pd, Material* tempHold)
 	{
@@ -214,6 +230,7 @@ public:
 			{
 				ary[index1] = -1;
 				ary[index2] = -1;
+				current -= 2;
 				return true;
 			}
 		}
@@ -237,6 +254,7 @@ public:
 			{
 				ary[index1] = -1;
 				ary[index2] = -1;
+				current -= 2;
 				return true;
 			}
 		}
@@ -261,6 +279,7 @@ public:
 			{
 				ary[index1] = -1;
 				ary[index2] = -1;
+				current -= 2;
 				return true;
 			}
 		}
@@ -272,35 +291,35 @@ public:
 		}
 	}
 	
-	Material pop()  // won't use in the future
-	{
-		Material item = -1; 
-		if(current > 0) 
-		{
-			for(int i =0; i != size; i++)
-			{
-				if(ary[i] != -1)
-				{
-					item = ary[i];
-					ary[i] = -1;
-					--current;
-					break;
-				}
-			}
-			if(item == MATERIAL_1)
-				m1_counter--;
-			else if(item == MATERIAL_2)
-				m2_counter--;
-			else if(item == MATERIAL_3)
-				m3_counter--;
-			return item;
-		}
-		else 
-		{ /* Error buffer empty */
-			return -1;
-		}
-		
-	}	
+	// Material pop()  // won't use in the future
+// 	{
+// 		Material item = -1; 
+// 		if(current > 0) 
+// 		{
+// 			for(int i =0; i != size; i++)
+// 			{
+// 				if(ary[i] != -1)
+// 				{
+// 					item = ary[i];
+// 					ary[i] = -1;
+// 					--current;
+// 					break;
+// 				}
+// 			}
+// 			if(item == MATERIAL_1)
+// 				m1_counter--;
+// 			else if(item == MATERIAL_2)
+// 				m2_counter--;
+// 			else if(item == MATERIAL_3)
+// 				m3_counter--;
+// 			return item;
+// 		}
+// 		else 
+// 		{ /* Error buffer empty */
+// 			return -1;
+// 		}
+// 		
+// 	}	
 	void showState()
 	{
 		printf("The state of InputBuffer Now is:\n");
@@ -392,6 +411,14 @@ public:
 	void push(Product pd)
 	{
 		ary[current++] = pd;
+		if(pd == PRODUCT_1)
+			p1_counter++;
+		else if (pd == PRODUCT_2)
+			p2_counter++;
+		else if(pd == PRODUCT_3)
+			p3_counter++;
+		else
+			printf("******* error: in push of outputqueue\n\n\n");
 	}
 		
 	
@@ -410,7 +437,7 @@ public:
 	{
 		if(pd == ary[current - 1])
 		{
-			printf("********* error: same product next to each other");
+			printf("********* error: same product next to each other\n\n\n");
 			g_outputQueueDeadlockCounter++;
 		}
 		else
@@ -421,7 +448,7 @@ public:
 					push(pd);
 				else
 				{
-					printf("invalid number constraint, dealLock occurs, discard this product_%d to make the process continue",pd);
+					printf("**********  error invalid number constraint, dealLock occurs, discard this product_%d to make the process continue\n\n\n",pd);
 					g_outputQueueDeadlockCounter++;
 				}
 				
@@ -433,7 +460,7 @@ public:
 					push(pd);
 				else
 				{
-					printf("invalid number constraint, dealLock occurs, discard this product_%d to make the process continue",pd);
+					printf("***************** error invalid number constraint, dealLock occurs, discard this product_%d to make the process continue\n\n\n",pd);
 					g_outputQueueDeadlockCounter++;
 				}
 				
@@ -445,7 +472,7 @@ public:
 					push(pd);
 				else
 				{
-					printf("invalid number constraint, dealLock occurs, discard this product_%d to make the process continue",pd);
+					printf("************ error invalid number constraint, dealLock occurs, discard this product_%d to make the process continue\n\n\n",pd);
 					g_outputQueueDeadlockCounter++;
 				}
 				
@@ -494,7 +521,7 @@ void initializeData(int nTools)
 	sem_init(&empty, 0, BUFFER_SIZE);
 	
 	sem_init(&tools,0, nTools/2); // in my strategy, the operator will whether fetch two tools or not fetch any tools, so, the last odd one is useless, by dividing the number of tools by 2, each down and up on semaphore means get or put back two tools
-	printf("there are %d tools \n\n\n\n",nTools/2);
+	//printf("there are %d tools \n\n\n\n",nTools/2);
 
 	/* Get the default attributes */
 	pthread_attr_init(&attr);
@@ -510,18 +537,18 @@ void *generator(void *param)
 	Material materialID = generatorID;  
 	// the materialID is equal to generatorID, which means the material generated by generator1 is 1(MATERIAL_1),etc 
 	while(TRUE) {
-		printf("$$$$$$$ pid is %d $$$$$$$$$$\n", getpid()); //////////////////
+		//printf("$$$$$$$ pid is %d $$$$$$$$$$\n", getpid()); //////////////////
 		/* sleep for a random period of time */
-		printf("Now in generator%d, sleep for a while\n",generatorID);
+		//printf("Now in generator%d, sleep for a while\n",generatorID);
 		int rNum = (rand() / RAND_DIVISOR)%100;
 		sleep(rNum);
-		printf("generator%d finish sleep.\n",generatorID);
+		//printf("generator%d finish sleep.\n",generatorID);
 		/* acquire the empty lock */
 		sem_wait(&empty);
-		printf("generator%d finish to down the semaphore.\n", generatorID);
+		//printf("generator%d finish to down the semaphore.\n", generatorID);
 		/* acquire the inputBuffer_mutex lock */
 		pthread_mutex_lock(&inputBuffer_mutex);
-		printf("generator%d enter the critical section.\n", generatorID);
+		//printf("generator%d enter the critical section.\n", generatorID);
 
 		if(inputBuffer.push(materialID) == -1) {
 			printf("************* error:generator%d report error condition\n", generatorID);
@@ -532,10 +559,10 @@ void *generator(void *param)
 		inputBuffer.showAry();
 		/* release the inputBuffer_mutex lock */
 		pthread_mutex_unlock(&inputBuffer_mutex);
-		printf("generator%d exits the critical section, and try to up the semaphore\n",generatorID);
+		//printf("generator%d exits the critical section, and try to up the semaphore\n",generatorID);
 		/* signal full */
 		sem_post(&full);
-		printf("generator%d finish up the semaphore\n",generatorID);
+		//printf("generator%d finish up the semaphore\n",generatorID);
 		
 	}
 	return NULL;
@@ -546,12 +573,12 @@ void *operators(void *param) {
 	int operatorsID = *(int*)param;
 	while(TRUE) {
 		Material temp_materials[2] = {-1,-1}; // -1 means no material holded 
-		printf("$$$$$$$ pid is %d $$$$$$$$$$\n", getpid()); /////////////
+		//printf("$$$$$$$ pid is %d $$$$$$$$$$\n", getpid()); /////////////
 		/* sleep for a random period of time */
-		printf("now in operators%d, sleep for a while\n",operatorsID);
+		//printf("now in operators%d, sleep for a while\n",operatorsID);
 		int rNum = (rand() / RAND_DIVISOR)%100;
 		sleep(rNum);
-		printf("operator%d finish sleep\n",operatorsID);
+		//printf("operator%d finish sleep\n",operatorsID);
 		printf("operator%d try to fetch the tools\n", operatorsID);
 		sem_wait(&tools); // aquire two tools
 		//printf("There are %d tools in process \n\n\n",(int)tools);
@@ -564,13 +591,14 @@ void *operators(void *param) {
 		printf("operators%d finish getting all the materials semaphores \n",operatorsID);
 		/* aquire the inputBuffer_mutex lock */
 		pthread_mutex_lock(&inputBuffer_mutex);
-		printf("operators%d enter the critical section\n",operatorsID);
+		//printf("operators%d enter the critical section\n",operatorsID);
 		Product priority[2]; //ask the outputQueue which product to operate first
 		outputQueue.check_priority_next(priority);
 		if(inputBuffer.try_get_materials(priority[0],temp_materials)) //$$$$$$$$$$$$ maybe don't need temp_materials
 		{
 			pthread_mutex_lock(&outputQueue_mutex);
 			outputQueue.try_insert_product(priority[0]);
+			printf("^^^^^^^ operator %d MAY produce the product%d\n",operatorsID,priority[0]);
 			pthread_mutex_unlock(&outputQueue_mutex);
 			sem_post(&empty);
 			sem_post(&empty);
@@ -580,6 +608,7 @@ void *operators(void *param) {
 		{
 			pthread_mutex_lock(&outputQueue_mutex);
 			outputQueue.try_insert_product(priority[1]);
+			printf("^^^^^^^ operator %d MAY produce the product%d\n",operatorsID,priority[1]);
 			pthread_mutex_unlock(&outputQueue_mutex);
 			sem_post(&empty);
 			sem_post(&empty);
@@ -589,11 +618,12 @@ void *operators(void *param) {
 		{
 			sem_post(&full);
 			sem_post(&full);
+			printf("^^^^^^^ operator_%d put the materials back to the inputBuffer\n\n",operatorsID);
 			//sem_post(&tools)
 		}
 		inputBuffer.showAry();
 		pthread_mutex_unlock(&inputBuffer_mutex);
-		printf("operators%d exit the critical section\n",operatorsID);
+		//printf("operators%d exit the critical section\n",operatorsID);
 		/* signal empty */
 		printf("operator%d put back the tools\n",operatorsID);
 		sem_post(&tools); //release the two tools
@@ -647,7 +677,7 @@ int main(int argc, char *argv[])
 			//changemode(1);
 			do
 			{
-				printf("############## pid is %d $$$$$$$$$$\n \n \n \n\n\n\n\n\n\n\n\n\n\n\n\n", getpid());
+				//printf("############## pid is %d $$$$$$$$$$\n \n \n \n\n\n\n\n\n\n\n\n\n\n\n\n", getpid());
 				// int ch;
 				// 
 				// while(TRUE) // ****** just for test need a sleep or counter in fact
@@ -698,8 +728,8 @@ void execute_child_process(int* args)
 	int numGeneator = 3; /* Number of generator threads  is a const 3 */
 	//int numTools = args[1]; // Number of tools
 	int numOperator = args[2]; /* Number of operators threads */
-	printf("----------------- now in main, before create the generator\n");
-	printf("$$$$$$$ pid is %d $$$$$$$$$$\n", getpid());///////////////
+	printf("----------------- now in main, before create the generator\n\n\n");
+	//printf("$$$$$$$ pid is %d $$$$$$$$$$\n", getpid());///////////////
 	/* Create the generator threads */
 	static int proCounter = 0;
 	for(int i = 0; i < numGeneator; i++) {
@@ -709,7 +739,7 @@ void execute_child_process(int* args)
 		proCounter++;
 		
 	}
-	printf("----------------- now in main, finish create the generator, before create the operators\n");
+	//printf("----------------- now in main, finish create the generator, before create the operators\n");
 	/* Create the operators threads */
 	static int conCounter = 0;
 	for(int i = 0; i < numOperator; i++) {
@@ -721,11 +751,14 @@ void execute_child_process(int* args)
 	}
 
 	/* Sleep for the specified amount of time in milliseconds */
-	printf("---------------- now in main , finish create operators and before main sleep\n");
+	//printf("---------------- now in main , finish create operators and before main sleep\n");
 	sleep(mainSleepTime);
 
 	/* Exit the program */
-	printf("Exit the program\n");
+	//printf("Exit the program\n");
+	show_material_total();
+	outputQueue.showStatus();
+	show_deadlock();
 	exit(0);
 	
 }
